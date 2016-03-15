e2f380ea18e5262a39ff1dd3aceeaa0139d36e54
Marina <marina.blin@free.fr>
2016-03-15 19:52:49 +0100

    Add Affiliation object

---
+++ src/xmlObject/Affiliation.java
@@ -0,0 +1,40 @@
+package xmlObject;
+
+import javax.xml.bind.annotation.XmlAttribute;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlValue;
+
+@XmlRootElement(name = "affiliation")
+public class Affiliation 
+{
+	private int id;
+	private String name;
+	
+	
+	public int getId() 
+	{
+		return id;
+	}
+	
+	public String getName() 
+	{
+		return name;
+	}
+	
+	@XmlAttribute(name = "affiliationId")
+	public void setId(int id) 
+	{
+		this.id = id;
+	}
+	
+	@XmlValue
+	public void setName(String name) 
+	{
+		this.name = name;
+	}
+	
+	public String toString()
+	{
+		return "Affiliation{affiliationId='" + this.id + "', name='" + this.name +"'}";
+	}
+}
