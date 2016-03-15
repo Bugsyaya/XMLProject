26a159e51367fd7e8b27e94881836bb71b8c178d
Marina <marina.blin@free.fr>
2016-03-15 19:52:11 +0100

    Add Affiliation object

--- src/xmlObject/Acceptation.java
+++ src/xmlObject/Acceptation.java
@@ -1,42 +1,49 @@
 package xmlObject;
 
-import javax.xml.bind.annotation.XmlAttribute;
-import javax.xml.bind.annotation.XmlValue;
+import javax.xml.bind.annotation.*;
 
-public class Acceptation {
+@XmlRootElement(name = "acceptations")
+public class Acceptation 
+{
 	private String id;
 	private int soumission;
 	private String name;
 	
-	public String getId() {
+	public String getId() 
+	{
 		return id;
 	}
 	
-	public int getSoumission() {
+	public int getSoumission() 
+	{
 		return soumission;
 	}
 	
-	public String getName() {
+	public String getName() 
+	{
 		return name;
 	}
 
 	@XmlAttribute(name = "id")
-	public void setId(String id) {
+	public void setId(String id) 
+	{
 		this.id = id;
 	}
 
 	@XmlAttribute(name = "soumission")
-	public void setSoumission(int soumission) {
+	public void setSoumission(int soumission) 
+	{
 		this.soumission = soumission;
 	}
 
 	@XmlValue
-	public void setName(String name) {
+	public void setName(String name) 
+	{
 		this.name = name;
 	}
 	
 	public String toString() 
 	{
-		return "Acceptations{" + "id='" + this.id + "', soumissions=" + this.soumission + ", name='" + this.name + "'}";
+		return "Acceptations{id='" + this.id + "', soumissions=" + this.soumission + ", name='" + this.name + "'}";
 	}
 }
