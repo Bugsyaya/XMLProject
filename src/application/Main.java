a2985e3269a34dad2a685dc2b8589ee34075c3bb
Marina <marina.blin@free.fr>
2016-03-20 12:26:28 +0100

    Refactor main

---
+++ src/application/Main.java
@@ -0,0 +1,46 @@
+package application;
+
+import java.util.ArrayList;
+
+import javax.xml.xpath.XPathExpressionException;
+
+import Parser.DomXPath;
+import xmlObject.Article;
+import xmlObject.Conference;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		String fileName = "xmlFile/TALN-RECITAL-BIB.xml";
+
+		System.out.println("Début des tests");
+		System.out.println("===============");
+		
+		
+		System.out.println();
+		System.out.println("Test du parser Sax");
+		System.out.println("=======================");
+		
+		
+		System.out.println();
+		System.out.println("Test du parser DomXPath");
+		System.out.println("=======================");
+		DomXPath parser = new DomXPath(fileName);
+		System.out.print("L'analyse du document peut prendre quelques secondes...");
+		try {
+			ArrayList<Conference> conferences = parser.buildConferences();
+			for (Conference c: conferences) {
+				System.out.println("Conférence : " + c.getEdition().getTitre());
+				for (Article a: c.getArticles()) {
+					System.out.println(" . " + a.getTitre());
+				}
+			}
+		} catch(XPathExpressionException e) {
+			System.out.println("Problème dans l'expression XPath : ");
+			System.out.println(e.getMessage());
+		}
+		
+	}
+
+}
