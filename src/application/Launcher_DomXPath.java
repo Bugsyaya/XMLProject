76bd4cffc0ce1c9963769420b5036ee3cd1aad76
Maël Cadorel <mael.cadorel@laposte.net>
2016-03-20 19:10:28 +0100

    Implement HTML generation in DomXPath launcher (not working)

--- src/application/Launcher_DomXPath.java
+++ src/application/Launcher_DomXPath.java
@@ -18,18 +18,24 @@ public class Launcher_DomXPath implements Launcher {
 		System.out.println("=======================");
 		DomXPath parser = new DomXPath(HtmlGenerate.XML_FILE_NAME);
 		System.out.print("L'analyse du document peut prendre quelques secondes...");
+		ArrayList<Conference> conferences = new ArrayList<>();
 		try {
-			ArrayList<Conference> conferences = parser.buildConferences();
-			for (Conference c: conferences) {
-				System.out.println("Conférence : " + c.getEdition().getTitre());
-				for (Article a: c.getArticles()) {
-					System.out.println(" . " + a.getTitre());
-				}
-			}
+			conferences = parser.buildConferences();
+//			for (Conference c: conferences) {
+//				System.out.println("Conférence : " + c.getEdition().getTitre());
+//				for (Article a: c.getArticles()) {
+//					System.out.println(" . " + a.getTitre());
+//				}
+//			}
 		} catch(XPathExpressionException e) {
 			System.out.println("Problème dans l'expression XPath : ");
 			System.out.println(e.getMessage());
 		}
+		
+	    if (conferences != null) {
+	        System.out.println(conferences.size() + " conférences ont été récupérées du fichier XML\n");
+	        HtmlGenerate.generateHomePage(conferences);
+	    }
 		
 	}
 
